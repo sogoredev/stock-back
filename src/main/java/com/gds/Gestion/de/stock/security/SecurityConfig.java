894e4a4c8163dad2d36fb32cbe4d841edbd3d48c
sogoredev <sogoredev@gmail.com>
2024-12-05 18:36:02 +0000

    set up proxy

--- src/main/java/com/gds/Gestion/de/stock/security/SecurityConfig.java
+++ src/main/java/com/gds/Gestion/de/stock/security/SecurityConfig.java
@@ -121,4 +121,17 @@ public class SecurityConfig {
         daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder);
         return daoAuthenticationProvider;
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://stock-proxy"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
